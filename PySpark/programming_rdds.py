efdd8d174e5eba97ea0a3945b99b9f9a8265de7d
jacheung <jacheung6@gmail.com>
2020-03-31 16:32:34 -0700

    finished programming rdds

--- PySpark/programming_rdds.py
+++ PySpark/programming_rdds.py
@@ -1,4 +1,16 @@
-from pyspark import SparkContext as sc
+from pyspark import SparkConf
+from pyspark.context import SparkContext
+sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
 
 # data source http://www.gutenberg.org/ebooks/100
-baseRDD = sc.textfile(file_path)
+baseRDD = sc.textFile('/Users/jonathancheung/Documents/GitHub/still-learning/PySpark/datasets/gutenberg_shakespeare.txt')
+
+splitRDD = baseRDD.flatMap(lambda x: x.split())  # using flatmap to change sentences into words RDD
+print("Total number of words in splitRDD:", splitRDD.count()) # Count the total number of words
+splitRDD_no_stop = splitRDD.filter(lambda x: x.lower() not in stop_words)  # lower case and remove stop
+splitRDD_no_stop_words = splitRDD_no_stop.map(lambda w: (w, 1))  # Create a tuple of the word and 1
+resultRDD = splitRDD_no_stop_words.reduceByKey(lambda x, y: x + y) # Count occurences of each word
+
+resultRDD_sort = resultRDD.sortByKey(ascending=False)  #sort keys in descending order
+for word in resultRDD_sort.take(10):  # Show the top 10 most frequent words and their frequencies
+    print("{} has {} counts". format(word[0], word[1]))
